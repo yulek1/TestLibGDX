7cd5a094f8d6d90078c62e108222d47a46f06f96
yulek1 <y.v.yadgarova@gmail.com>
2014-07-09 09:36:07 +0400

    tweenEngine

---
+++ TweenEngine/src/rdo/testing/tween/SpriteAccessor.java
@@ -0,0 +1,52 @@
+package rdo.testing.tween;
+
+import com.badlogic.gdx.graphics.Color;
+import com.badlogic.gdx.graphics.g2d.Sprite;
+import com.badlogic.gdx.graphics.g2d.SpriteBatch;
+
+import aurelienribon.tweenengine.Tween;
+import aurelienribon.tweenengine.TweenAccessor;
+import aurelienribon.tweenengine.TweenManager;
+import aurelienribon.tweenengine.equations.Bounce;
+
+public class SpriteAccessor implements TweenAccessor<Sprite> {
+	public static final int POS_XY = 1;
+	public static final int COLOR = 2;
+	 
+    @Override
+    public int getValues(Sprite target, int tweenType, float[] returnValues) {
+        switch (tweenType) {
+            case POS_XY:
+            	returnValues[0] = target.getX();
+            	returnValues[1] = target.getY();            	
+            	return 2;
+            case COLOR:
+            	returnValues[0] = target.getColor().r;
+            	returnValues[1] = target.getColor().g;
+            	returnValues[2] = target.getColor().b;
+            	returnValues[3] = target.getColor().a;                 
+            	return 4;
+            default:
+                assert false;
+                return -1;
+         
+        }
+    }
+ 
+    @Override
+    public void setValues(Sprite target, int tweenType, float[] newValues) {
+        switch (tweenType) {
+        case POS_XY:
+        	target.setPosition(newValues[0], newValues[1]);
+        	break;
+        case COLOR:
+        	Color c = target.getColor();
+        	c.set(newValues[0], newValues[1], newValues[2], newValues[3]);
+        	target.setColor(c);
+        	break;
+        default:
+        	assert false;
+        		
+        }
+    }
+}
---
+++ TweenEngine/src/rdo/testing/tween/TweenApplication.java
@@ -0,0 +1,136 @@
+package rdo.testing.tween;
+
+import aurelienribon.tweenengine.BaseTween;
+import aurelienribon.tweenengine.Timeline;
+import aurelienribon.tweenengine.Tween;
+import aurelienribon.tweenengine.TweenCallback;
+import aurelienribon.tweenengine.TweenEquations;
+import aurelienribon.tweenengine.TweenManager;
+import aurelienribon.tweenengine.equations.Bounce;
+
+import com.badlogic.gdx.ApplicationListener;
+import com.badlogic.gdx.Gdx;
+import com.badlogic.gdx.backends.lwjgl.LwjglApplication;
+import com.badlogic.gdx.graphics.Camera;
+import com.badlogic.gdx.graphics.GL20;
+import com.badlogic.gdx.graphics.OrthographicCamera;
+import com.badlogic.gdx.graphics.PerspectiveCamera;
+import com.badlogic.gdx.graphics.Texture;
+import com.badlogic.gdx.graphics.Texture.TextureFilter;
+import com.badlogic.gdx.graphics.g2d.Sprite;
+import com.badlogic.gdx.graphics.g2d.SpriteBatch;
+import com.badlogic.gdx.math.MathUtils;
+import com.badlogic.gdx.scenes.scene2d.Stage;
+import com.badlogic.gdx.utils.TimeUtils;
+import com.badlogic.gdx.utils.viewport.FitViewport;
+import com.badlogic.gdx.utils.viewport.Viewport;
+
+public class TweenApplication implements ApplicationListener {
+	
+	 	private OrthographicCamera camera;
+	    private SpriteBatch batch;
+	    private Texture texture;
+	    private Texture CircTxt, ElasticTxt, QuadTxt; //** text **//
+	    private Sprite sprite1;
+	    private TweenManager tweenManager;
+
+
+	    public void setTweenManager(TweenManager tm){
+	    	this.tweenManager = tm;
+	    }
+	    
+	       float screenW = 480;
+	        float screenH = 480;
+	        float w = 1;
+	        float h = w * screenH / screenW;
+	    
+	@Override
+	public void create() {
+		
+		setTweenManager(new TweenManager());
+		
+		camera = new OrthographicCamera(w, h);		
+        batch = new SpriteBatch();
+        texture = new Texture(Gdx.files.internal("data/test.png"));
+        texture.setFilter(TextureFilter.Linear, TextureFilter.Linear);
+        sprite1 = new Sprite(texture);
+        sprite1.setPosition(-w/2, -h/2);
+        
+        Tween.registerAccessor(Sprite.class, new SpriteAccessor());
+        
+        Timeline timeline = Timeline.createParallel()
+                .beginSequence()
+                .push(Tween.to(sprite1, SpriteAccessor.POS_XY, 3).target(100, 200))
+                .push(Tween.to(sprite1, SpriteAccessor.POS_XY, 2).target(200, 200))
+                .push(Tween.to(sprite1, SpriteAccessor.POS_XY, 3).target(400, 200))
+                .push(Tween.to(sprite1, SpriteAccessor.POS_XY, 3).target(400, 400))
+                .end()
+                .start(this.tweenManager);
+                
+        
+       // Tween.call(windCallback).start(tweenManager);
+	}
+
+	@Override
+	public void dispose() {
+		batch.dispose();
+        texture.dispose();
+        //font.dispose();
+
+	}
+
+	@Override
+	public void pause() {
+		// TODO Auto-generated method stub
+
+	}
+
+	@Override
+	public void render() {
+		   Gdx.gl.glClearColor(1, 1, 1, 1);
+	       Gdx.gl.glClear(GL20.GL_COLOR_BUFFER_BIT);
+	 
+	        batch.begin();
+	        sprite1.draw(batch);
+	        batch.end();
+	        
+	        
+	        tweenManager.update(Gdx.graphics.getDeltaTime());
+	        
+
+        
+
+	}
+
+	@Override
+	public void resize(int arg0, int arg1) {
+		// TODO Auto-generated method stub
+
+	}
+
+	@Override
+	public void resume() {
+		// TODO Auto-generated method stub
+
+	}
+
+	
+    public static void main(String[] args){
+    	
+    	new LwjglApplication(new TweenApplication());
+    }
+    
+    private final TweenCallback windCallback = new TweenCallback() {
+        @Override
+        public void onEvent(int type, BaseTween<?> source) {
+            float d = MathUtils.random() * 0.5f + 0.5f;   // duration
+            
+            Tween.to(sprite1, SpriteAccessor.POS_XY, d)
+                .target(100, 200)
+                .repeatYoyo(1, 0)
+                .setCallback(windCallback)
+                .start(tweenManager);
+            
+        }
+    };
+}
