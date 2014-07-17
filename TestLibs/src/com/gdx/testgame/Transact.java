a5a4380fced4a0368bfacce4d1624c05447858dd
yulek1 <y.v.yadgarova@gmail.com>
2014-07-17 21:30:11 +0400

    - убрано лишнее

--- TestLibs/src/com/gdx/testgame/Transact.java
+++ TestLibs/src/com/gdx/testgame/Transact.java
@@ -7,13 +7,11 @@ import com.badlogic.gdx.scenes.scene2d.Actor;
 
 public class Transact extends Actor {
 		
-	Texture texture = new Texture(Gdx.files.internal("data/test.png"));
-	public boolean started = false;
-	
+	Texture texture = new Texture(Gdx.files.internal("data/test.png"));	
 
 	@Override
 	public void draw(Batch batch, float alpha) {
-		batch.draw(texture, this.getX(), getY());
+		batch.draw(texture, getX(), getY());
 	}
 
 }
