a523bbd9f6f7de3e05c407b788a70d92e1e379bb
yulek1 <y.v.yadgarova@gmail.com>
2014-07-19 18:41:45 +0400

    - попыталась выровнять скорость

--- TestLibs/src/com/gdx/testgame/PathGame.java
+++ TestLibs/src/com/gdx/testgame/PathGame.java
@@ -1,12 +1,22 @@
 package com.gdx.testgame;
 
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Random;
+
 import com.badlogic.gdx.ApplicationListener;
 import com.badlogic.gdx.Gdx;
 import com.badlogic.gdx.backends.lwjgl.LwjglApplication;
 import com.badlogic.gdx.graphics.Camera;
 import com.badlogic.gdx.graphics.GL20;
 import com.badlogic.gdx.graphics.OrthographicCamera;
+import com.badlogic.gdx.math.Polyline;
+import com.badlogic.gdx.math.Rectangle;
+import com.badlogic.gdx.math.Vector2;
+import com.badlogic.gdx.math.Vector3;
 import com.badlogic.gdx.scenes.scene2d.actions.Actions;
+import com.badlogic.gdx.scenes.scene2d.actions.MoveToAction;
+import com.badlogic.gdx.scenes.scene2d.actions.SequenceAction;
 
 public class PathGame implements ApplicationListener {
 
@@ -15,28 +25,22 @@ public class PathGame implements ApplicationListener {
 	
 	private float XPos; // coeff screen width;
 	private float YPos; // coeff screen height;
-
+	
 	@Override
 	public void create() {
-		
-		camera = new OrthographicCamera(Gdx.graphics.getWidth(), Gdx.graphics.getHeight());
-		camera.position.set(Gdx.graphics.getWidth() / 2, Gdx.graphics.getHeight() / 2, 0);
-		
-		int duration = 3;
 		
 		normalizeGraphics();
 		
 		world = new World();
+		
 		Gdx.input.setInputProcessor(world);		
-
+		
 		Transact transact = world.getTransact();
+		
+		Line[] lineArray = this.generateRandomPath(20);
+		int speed = 20;
 
-		transact.addAction(Actions.sequence(Actions.moveTo(0.5f*XPos, 0, duration),
-				Actions.moveTo(0.5f*XPos, 1f*YPos, duration), 
-				Actions.moveTo(0.2f*XPos, 0.2f*YPos, duration),
-				Actions.moveTo(0.3f*XPos, 0.75f*YPos, duration),
-				Actions.moveTo(0.48f*XPos, 0.200f*YPos, duration),
-				Actions.moveTo(0.480f*XPos, 0.320f*YPos, duration)));
+		transact.addAction(this.generateSequence(lineArray, speed));
 
 		world.addActor(transact);
 	}
@@ -81,11 +85,77 @@ public class PathGame implements ApplicationListener {
 
 	}
 	
+	private class Line{
+		private float X1, X2;
+		private float Y1, Y2;
+		
+		private Line previousLine;
+		
+		public Line(float X1, float Y1, float X2, float Y2){
+			this.X1 = X1;
+			this.Y1 = Y1;
+			
+			this.X2 = X2;
+			this.Y2 = Y2;
+			
+			this.previousLine = null;
+		}
+		
+		public Line(Line prevLine, float X, float Y){
+			this.X1 = prevLine.getX2();
+			this.Y1 = prevLine.getY2();
+			this.X2 = X;
+			this.Y2 = Y;
+			
+			this.previousLine = prevLine;
+		}
+		
+		public float getX1(){ return X1; }
+		public float getY1(){ return Y1; }
+		public float getX2(){ return X2; }
+		public float getY2(){ return Y2; }
+		
+	}
+	
+	private Line[] generateRandomPath(int size){
+		Line[] lineArray = new Line[size];
+		Random rnd = new Random();
+		rnd.setSeed(System.currentTimeMillis());
+		lineArray[0] = new Line(rnd.nextFloat(), rnd.nextFloat(), rnd.nextFloat(), rnd.nextFloat());
+		for( int i = 1; i < lineArray.length - 1; i++){
+			lineArray[i] = new Line(lineArray[i-1], rnd.nextFloat(), rnd.nextFloat());
+		}
+		return lineArray;
+	}
+	
+	private SequenceAction generateSequence(Line[] lineArray, float speed){
+		
+		SequenceAction sa = new SequenceAction();
+				
+		MoveToAction[] mta = new MoveToAction[lineArray.length];
+		double duration[] = new double[lineArray.length];
+		for ( int i = 1; i < lineArray.length - 1; i++){
+			double path = 100 * Math.sqrt(Math.pow((lineArray[i].getX2() - lineArray[i-1].getX1()), 2) + Math.pow((lineArray[i].getY2() - lineArray[i].getY1()), 2));
+			duration[i-1] = path/speed;
+			mta[i] = new MoveToAction();
+			mta[i].setX(lineArray[i].getX2()*XPos);
+			mta[i].setY(lineArray[i].getY2()*YPos);
+			mta[i].setDuration((float)duration[i-1]);
+			
+			sa.addAction(mta[i]);
+		}
+		
+		return sa;
+	}
+	
 	private void normalizeGraphics(){
 		
 		float CW = 1f; 
 		float CH = 1f;
 		
+		camera = new OrthographicCamera(CW, -CH);
+		camera.position.set(CW / 2, CH / 2, 0);
+				
 		CH =  CW* Gdx.graphics.getHeight()/Gdx.graphics.getWidth();
 		
 		float ppuX = (float)Gdx.graphics.getWidth() / CW;
